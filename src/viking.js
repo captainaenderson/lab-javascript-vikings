a5031248a2347e315ceb24b62e79e428522184a3
Ann-Kathrin Wohlrab <ann-kathrinwohlrab@imacvonnkathrin.speedport.ip>
2023-02-18 09:00:49 +0100

    almost done

--- src/viking.js
+++ src/viking.js
@@ -27,18 +27,53 @@ class Viking extends Soldier{
     receiveDamage(damage){
         this.health -= damage;
         if(this.health > 0){
-            return `${this.name} has received ${damage} points of damage`
+            return `${this.name} has received ${damage} points of damage`;
         } else {
-            return `${this.name} has died in act of combat`
+            return `${this.name} has died in act of combat`;
         }
     }
 
+    battleCry(){
+        return "Odin Owns You All!";
+    }
 }
 
 
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier{
+    attack(){
+        return this.strength;
+    }
+
+    receiveDamage(damage){
+        this.health -= damage;
+        if(this.health > 0){
+            return `A Saxon has received ${damage} points of damage`;
+        } else {
+            return `A Saxon has died in combat`;
+        }
+    }
+}
 
 // War
-class War {}
+class War {
+    vikingArmy = [];
+    saxonArmy = [];
+
+    addViking(viking){
+        this.vikingArmy.push(viking)
+    }
+    addSaxon(saxon){
+        this.saxonArmy.push(saxon)
+    }
+    vikingAttack() {
+
+    }
+    saxonAttack(){
+
+    }
+    showStatus(){
+
+    }
+}
